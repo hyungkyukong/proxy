cbee90e7345d934145b7982a228cebf24a1bb9e0
hkkong <konguuu@gmail.com>
2023-06-20 22:25:56 +0900

    프록시 패턴- 예제 코드1

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CachProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,15 @@ public class ProxyPatternTest {
         client.execute();
         client.execute();
 
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CachProxy cachProxy = new CachProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cachProxy);
+        client.execute();
+        client.execute();
+        client.execute();
     }
 }
