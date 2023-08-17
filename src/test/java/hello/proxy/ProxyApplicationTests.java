95b95107d62c21bb39a63589da6259d25a46a10d
hkkong <konguuu@gmail.com>
2023-08-17 23:49:35 +0900

    JDK 동적 프록시 - 적용2

--- src/test/java/hello/proxy/ProxyApplicationTests.java
+++ src/test/java/hello/proxy/ProxyApplicationTests.java
@@ -1,13 +1,37 @@
 package hello.proxy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//@SpringBootTest
+@Slf4j
 class ProxyApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+
+
+	@Test
+	void tempTest() {
+
+		Iterator<String> currentArgument;
+		List<String> argsList = new ArrayList<>();
+
+		argsList.add("a");
+		argsList.add("b");
+		argsList.add("c");
+		log.info("devlog iterator : {}", argsList.iterator());
+		for (currentArgument = argsList.iterator(); currentArgument.hasNext(); ) {
+			String arg = currentArgument.next();
+			log.info("devlog currentArgument : {}", arg);
+		}
 	}
+
 
 }
--- src/test/java/hello/proxy/jdkdynamic/code/BInterface.java
+++ src/test/java/hello/proxy/jdkdynamic/code/BInterface.java
@@ -3,3 +3,5 @@ package hello.proxy.jdkdynamic.code;
 public interface BInterface {
     String call();
 }
+
+
